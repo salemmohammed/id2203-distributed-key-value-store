e84369d6227118923e8620858740bf19f2485027
robzor92 <Sverige92>
2016-02-12 11:44:43 +0100

    added comments and more code for EPFD

--- src/main/java/system/Node.java
+++ src/main/java/system/Node.java
@@ -11,8 +11,10 @@ import se.sics.kompics.timer.CancelPeriodicTimeout;
 import se.sics.kompics.timer.SchedulePeriodicTimeout;
 import se.sics.kompics.timer.Timer;
 import se.sics.kompics.timer.Timeout;
+import system.event.HeartbeatMessage;
 import system.event.NodeMessage;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Node extends ComponentDefinition {
@@ -24,34 +26,47 @@ public class Node extends ComponentDefinition {
 
 
     private final HashMap<String, TAddress> neighbours;
+    private final ArrayList<TAddress> suspectedNodes;
+    private final ArrayList<TAddress> aliveNodes;
     Positive<Network> net = requires(Network.class);
     Positive<Timer> timer = requires(Timer.class);
     private UUID timerId;
+    private int heartbeatDelay = 500;
+    private int heartbeatDelayIncrease = 200;
 
     private LinkedList<TAddress> startupAcks;
 
 
     public Node(Init init) {
+        suspectedNodes = new ArrayList<>();
+        aliveNodes = new ArrayList<>();
         this.self = init.self;
         this.neighbours = init.neighbours;
         this.otherGroupLeader = init.otherGroupLeader;
         this.isLeader = init.isLeader;
-        startupAcks = new LinkedList<TAddress>();
+        startupAcks = new LinkedList<>();
         subscribe(startHandler, control);
         subscribe(nodeMessageHandler, net);
+        subscribe(heartbeatTimeoutHandler, timer);
     }
 
     Handler<Start> startHandler = new Handler<Start>() {
 
                 @Override
                 public void handle(Start event) {
-
-                    SchedulePeriodicTimeout spt = new SchedulePeriodicTimeout(0, 1000);
+                    //Init period timeout period
+                    int period = 1000;
+                    int delay = 500;
+                    SchedulePeriodicTimeout spt = new SchedulePeriodicTimeout(delay, period);
                     HeartbeatTimeout timeout = new HeartbeatTimeout(spt);
                     spt.setTimeoutEvent(timeout);
                     trigger(spt, timer);
                     timerId = timeout.getTimeoutId();
 
+                    //Add all neighbours as alive nodes
+                    aliveNodes.addAll(neighbours.values());
+
+                    //Send initial message to verify connectivity
                     Iterator it = neighbours.entrySet().iterator();
                     LOG.info( self.toString() + ": ( Start Event Triggered )") ;
                     while(it.hasNext()) {
@@ -59,16 +74,60 @@ public class Node extends ComponentDefinition {
                         trigger(new NodeMessage(self,(TAddress) pair.getValue()), net);
                         LOG.info( self.toString() + ": ( Node message sent To: " + pair.getValue() + " )");
                     }
+        }
+    };
+
+    Handler<HeartbeatTimeout> heartbeatTimeoutHandler = new Handler<HeartbeatTimeout>() {
+
+        @Override
+        public void handle(HeartbeatTimeout heartbeatTimeout) {
+
+            ArrayList<TAddress> intersection = new ArrayList<>(aliveNodes);
+            intersection.retainAll(suspectedNodes);
+            //If we found an alive node that is also suspected, we know need to wait longer for heartbeat reply
+            //This will prevent us from suspecting nodes that are alive simply because they did not reply to heartbeat
+            if(!intersection.isEmpty()) {
+                heartbeatDelay += heartbeatDelayIncrease;
+            }
+            //For each known neighbour
+            neighbours.entrySet().forEach(node -> {
+                TAddress nodeAddress = node.getValue();
+                //If we found a node that has not replied to last heartbeat and is not suspected
+                //We suspect it may have crashed and send a heartbeat to that node
+                if(!aliveNodes.contains(nodeAddress) && !suspectedNodes.contains(nodeAddress)){
+                    suspectedNodes.add(nodeAddress);
+                    //Here we trigger program logic for detection of suspected node
+                    System.out.println(self.toString() + ": SUSPECT: " + nodeAddress.toString());
+                }
+                //If we found a node that replied to heartbeat and is also suspected we consider it alive
+                else if(aliveNodes.contains(nodeAddress) && suspectedNodes.contains(nodeAddress)){
+                    suspectedNodes.remove(nodeAddress);
+                    //Here we trigger program logic for restore of suspected node
+                    System.out.println(self.toString() + ": RESTORE: " + nodeAddress.toString());
+                }
+                //Send a new heartbeat
+                trigger(new HeartbeatMessage(self, nodeAddress), net);
+            });
+            aliveNodes.clear();
+            //Subscribe to new timeout
+        }
+    };
+
+
+    //TODO add heartbeat request handler and reply
+    Handler<HeartbeatMessage> heartBeatHandler = new Handler<HeartbeatMessage>() {
+        @Override
+        public void handle(HeartbeatMessage heartbeatTimeout) {
+
 
         }
     };
 
 
-
     Handler<NodeMessage> nodeMessageHandler = new Handler<NodeMessage>() {
-
         @Override
         public void handle(NodeMessage event) {
+            //Leader waits for message from everyone in the group, then sends message to other leader
             if(isLeader) {
                 if(event.getSource().equals(otherGroupLeader)) {
                     LOG.info("Leader: " + self.toString() + event.getSource() + " Received From: " + event.getSource() + " )");
@@ -79,14 +138,10 @@ public class Node extends ComponentDefinition {
                 trigger(new NodeMessage(self,otherGroupLeader), net);
             }
             LOG.info( self.toString() + ": ( NodeMessage Received From: " + event.getSource() + " )");
-
         }
-
-
     };
 
     public static class HeartbeatTimeout extends Timeout {
-
         public HeartbeatTimeout(SchedulePeriodicTimeout spt) {
             super(spt);
         }
